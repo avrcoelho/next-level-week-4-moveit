efc6027424ac0e41a573ff14cbf5f299bac4e74d
avrcoelho <andrevrcoelho@hotmail.com>
2021-02-23 12:52:07 -0300

    feat next app and component profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import styles from "../styles/components/Profile.module.css";
+
+export const Profile = () => {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/avrcoelho.png" alt="André Coelho" />
+      <div>
+        <strong>André Coelho</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+};
